71aad54435e3e097b80bc82fa74a456f30f50a06
schroezp <schroezp@miamioh.edu>
2021-12-29 16:49:44 -0500

    Working on menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 const Header = () => {
     return (
-        <div className="navbar row" id="test">
-            <div className="col-8 navTitle"><a href="/">ZPS</a></div>
-            <a href="/"><div className="col-1 navLink">Home</div></a>
-            <a href="/school"><div className="col-1 navLink">School</div></a>
-            <a href="/current"><div className="col-1 navLink">Current</div></a>
-            <a href="/about"><div className="col-1 navLink">About</div></a>
+        <div>
+            <input id="menuButton" onclick="toggleMenu()" type="image" src="/images/nav.png" />
+
+            <div className="navbar row" id="test">
+                <div className="col-8 navTitle"><a href="/">ZPS</a></div>
+                <a href="/"><div className="col-1 navLink">Home</div></a>
+                <a href="/school"><div className="col-1 navLink">School</div></a>
+                <a href="/current"><div className="col-1 navLink">Current</div></a>
+                <a href="/about"><div className="col-1 navLink">About</div></a>
+            </div>
         </div>
     )
 }
