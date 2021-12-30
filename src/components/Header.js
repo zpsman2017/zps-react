86870cfbc9bc7afb7f47e8e001f7587d329af9b1
schroezp <schroezp@miamioh.edu>
2021-12-30 11:34:18 -0500

    Navbar toggling works on mobile. NoPage now has a contentDiv which fixed styling of div

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import { Outlet } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+
+// For mobile devices
+// Toggles navbar
+function toggleMenu() {
+    var links = document.getElementsByClassName("navLink");
+
+    for (let l of links) {
+        if (l.style.display == "initial")
+            l.style.display = "none";
+        else
+            l.style.display = "initial";
+    }
+}
 
 const Header = () => {
     return (
         <div>
-            <input id='menuButton' type='image' src='/images/nav.png' />
+            <FaBars id='menuButton' color='white' onClick={toggleMenu} />
 
             <div className='navbar row' id='test'>
                 <div className='col-8 navTitle'><a href='/'>ZPS</a></div>
@@ -12,7 +26,7 @@ const Header = () => {
                 <a href='/current'><div className='col-1 navLink'>Current</div></a>
                 <a href='/about'><div className='col-1 navLink'>About</div></a>
             </div>
-            <Outlet/>
+            <Outlet />
         </div>
     )
 }
--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,5 @@
 const NoPage = () => {
-    return <div class="row top">
+    return <div class="col-12 contentDiv">
         <div class="col-3 filler-top"></div>
         <div class="col-6 headerDiv">
             <h1>Sorry, that page doesn't exist</h1>
