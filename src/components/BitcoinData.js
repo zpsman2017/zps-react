b3bee9955877425495cf8475a1e3544815e18ac3
schroezp <schroezp@miamioh.edu>
2021-12-31 11:58:15 -0500

    Got latestHash working, as well as genesis block

--- src/components/BitcoinData.js
+++ src/components/BitcoinData.js
@@ -4,17 +4,18 @@ export default class BitcoinData extends React.Component {
 
     constructor(props) {
         super(props);
+        console.log(props.hash);
         this.state = {
             error: null,
             isLoaded: false,
-            hash: null,
+            hash: props.hash,
             height: null,
             transactions: null
         };
     }
 
-    componentDidMount() {
-        fetch("https://blockchain.info/rawblock/00000000000000000007982d9735957a5cb44f0785fa02b87d3160da50e78a45")
+    getBitcoinData(hash) {
+        fetch("https://blockchain.info/rawblock/" + hash)
             .then(res => res.json())
             .then(
                 (result) => {
@@ -34,22 +35,42 @@ export default class BitcoinData extends React.Component {
             )
     }
 
+    componentDidMount() {
+
+        if (this.props.hash == null) {
+            fetch("https://blockchain.info/q/latesthash")
+                .then(res => res.text())
+                .then(
+                    (result) => {
+                        this.getBitcoinData(result);
+                    },
+                    (error) => {
+                        this.getBitcoinData("");
+                    }
+                )
+        }
+        else {
+            this.getBitcoinData(this.props.hash);
+        }
+
+    }
+
     render() {
         const { error, isLoaded, hash, height, transactions } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
-            return <tbody>
+            return <table><tbody>
                 <tr><td class="table-label">Hash:</td><td class="table-data">Loading...</td></tr>
                 <tr><td class="table-label">Height:</td><td class="table-data">Loading...</td></tr>
                 <tr><td class="table-label">Transactions:</td><td class="table-data">Loading...</td></tr>
-            </tbody>;
+            </tbody></table>;
         } else {
-            return <tbody>
+            return <table><tbody>
                 <tr><td class="table-label">Hash:</td><td class="table-data">{hash}</td></tr>
                 <tr><td class="table-label">Height:</td><td class="table-data">{height}</td></tr>
                 <tr><td class="table-label">Transactions:</td><td class="table-data">{transactions}</td></tr>
-            </tbody>;
+            </tbody></table>;
         }
     }
 
--- src/pages/Bitcoin.js
+++ src/pages/Bitcoin.js
@@ -1,5 +1,4 @@
 import BitcoinData from '../components/BitcoinData'
-import LatestHash from '../components/LatestHash';
 
 const About = () => {
     return <div class="col-12 contentDiv">
@@ -13,10 +12,21 @@ const About = () => {
         <div class="row top">
             <div class="col-2 filler-top"></div>
             <div class="col-8 headerDiv">
-                <h2>Most Recent Bitcoin Block</h2> 
+                <h2>Most Recent Bitcoin Block</h2>
                 <div class="col-1 filler-top"></div>
                 <div class="col-10 paraDiv">
-                        <BitcoinData />
+                    <BitcoinData />
+                </div>
+            </div>
+        </div>
+
+        <div class="row top">
+            <div class="col-2 filler-top"></div>
+            <div class="col-8 headerDiv">
+                <h2>Bitcoin Genesis Block</h2>
+                <div class="col-1 filler-top"></div>
+                <div class="col-10 paraDiv">
+                    <BitcoinData hash='000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f' />
                 </div>
             </div>
         </div>
